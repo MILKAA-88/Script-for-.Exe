d6cbd0864447eca44bbf6ef13318875b7a1add57
MILKAA <kameel.2011@icloud.com>
2025-10-26 17:20:59 +0100

    Improved the security

--- main.js
+++ main.js
@@ -7,18 +7,19 @@ function createWindow() {
     height: 600,
     icon: path.join(__dirname, 'icon.ico'),
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
+      nodeIntegration: false, // Attention ! Ne changez pas, si vous mettez "true", vous vous exposez à des failles de sécurité (voir README)
+      contextIsolation: true, // Même chose
+      preload: path.join(__dirname, 'preload.js'), // Fichier pour exposer des API de manière sécurisée
     },
   });
-  
-  win.setMenuBarVisibility(false); // Cache la barre de menu 
+
+  win.setMenuBarVisibility(false); // Cache la barre de menu
   win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
   createWindow();
-  
+
   app.on('activate', () => {
     // Recrée une fenêtre si aucune n’est ouverte (macOS)
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
